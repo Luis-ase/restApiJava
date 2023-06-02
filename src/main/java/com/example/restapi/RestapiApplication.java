44ca65f8708d0cf47a9b19af484671492762285b
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-06-02 16:34:07 -0300

    Practi Spring-java 01

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -2,12 +2,18 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class RestapiApplication {
-
+	@GetMapping("/")
+	public String helloWorld(){
+		return "Hola Mundo con Spring";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
+
 
 }
